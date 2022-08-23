123afd09b8ac58d77f41ee91732841d83adc0c4b
kikinacademy <kikinacademy@gmail.com>
2022-08-23 18:53:33 -0500

    tarea2

---
+++ P1/Tarea2Regs/Empresa.cpp
@@ -0,0 +1,5 @@
+//
+// Created by eubgt on 8/23/22.
+//
+
+#include "Empresa.h"
---
+++ P1/Tarea2Regs/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+//Mostrar el arreglo.
+//
+//Ordenar alfabéticamente el arreglo, de la A a la Z, por el método de la burbuja y mostrarlo.
+//
+//Ofrecer las opciones insertar y borrar, con base en el nombre.
