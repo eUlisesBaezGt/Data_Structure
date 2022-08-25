06b7990e1c7f215f5aa9ec4426c484f2f1271c87
kikinacademy <kikinacademy@gmail.com>
2022-08-24 22:56:49 -0500

    Create RECAP1.cpp

---
+++ RECAP1.cpp
@@ -0,0 +1,103 @@
+
+Datos simples: un solo valor a la vez
+
+Datos estructurados: varios valores a la vez
+
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+
+Estructuras de datos:. colección de datos que se caracterizan por una
+organización y las operaciones que se definen en ellas.
+
+TDA (Tipo de datos abstractos): Diseño de una estructura, basado en la abstracción.
+
+ABSTRACCIÓN; considerar de forma independiente elementos unidos, para solo contemplar los indispensables.
+
+ABSTRACCIÓN DE DATOS: representar características básicas de una estructura de acuerdo a una serie de reglas.
+
+Colecciones de variables homogéneas o heterogéneas.
+
+Funciones principales: introducir, procesar, mantener, recuperar
+
+SIRVEN: para organizar información de manera eficiente en el diseño más completos para solucionar un problema.
+
+USOS: manejo dinámico de memoria, recuperar información, almacenar grandes conjuntos de datos
+
+Forma más fácil: struct
+
+Clasificación:
+- Estáticas: arreglos
+- Dinámicas: -lineales: pilas, colas
+             - no lineales: árboles, grafos
+
+Arreglos: colección ordenada de datos que se emplean para almacenar valores de un mismo tipo.
+
+Pilas: estructura en la que se accede vía LIFO (Last In First Out)
+
+Colas: estructura en la que se accede vía FIFO (First In First Out), y por alguno de los extremos.
+
+Árboles: estructura que representa datos con una relación jerárquica (predecesor y sucesores)
+
+Grafos: estructura formada por un conjunto de puntos y líneas, que unen a estos.------------------
+
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+
+    1. TDA y Estructuras de Datos
+2. Arreglos
+3. Registros
+    4. Recursión
+5. Pilas
+6. Colas
+7. Listas
+8. Árboles
+9. Grafos
+
+ESPECIFICACIÓN LÓGICA DE UN TDA
+1. Tipo de datos
+2. Organización
+3. Operaciones
+
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+
+INSERTAR:
+1. Revisar capacidad
+2. Localizar posicion
+3. Desplazar a la derecha
+4. Insertar en posición
+5. Actualizar capacidad
+
+BORRAR:
+1. Localizar posicion
+2. En caso falso: informar/terminar
+3. Desplazar a la izquierda. Posición siguiente y hasta ocupados-1
+4. Limpiar posición
+5. Actualizar capacidad
+
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+
+ARREGLO: dato estructurado que se emplea para almacenar valores de un mismo tipo en una misma variable.
+Finito, homogéneo y ordenado. tipo nombre [tam]
+Localidades contiguas de memoria.
+1. Lectura
+2. Asignación
+3. Modificación
+4. Inserción
+5. Borrado
+6. Busqueda
+7. Ordenación
+
+ARREGLO BIDIMENSIONAL: dato estructurado para tener varios datos con el mismo nombre.
+2 ÍNDICES
+
+Valores organizados en forma de matriz, renglones y columnas. Se
+
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+
+REGISTROS: dato estructurado formado por un conjunto limitado de elementos, sin importar el tipo. En
+Elemento = campo.
+
+Nombre, NO posicion
