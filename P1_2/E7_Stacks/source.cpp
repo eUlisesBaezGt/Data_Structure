ebe73ffa7b70b8de0847653999794b6f5efba7cb
kikinacademy <kikinacademy@gmail.com>
2022-09-13 18:25:01 -0500

    E7

--- P1_2/E7_Stacks/source.cpp
+++ P1_2/E7_Stacks/source.cpp
@@ -3,45 +3,47 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     Stack s;
     Employee e;
     s.isEmpty();
 
     int choice;
 
-    do
-    {
+    do {
+        cout << endl << "------------- MAIN MENU ------------- " << endl;
         cout << "1. Push" << endl;
         cout << "2. Pop" << endl;
         cout << "3. Print" << endl;
         cout << "0. Exit" << endl;
         cout << "Enter your choice: ";
         cin >> choice;
+        cout << endl;
 
-        switch (choice)
-        {
-        case 1:
-            cout << "Hiring" << endl;
-            cout << "Enter ID: ";
-            cin >> e.ID;
-            cout << "Enter Name: ";
-            cin >> e.Name;
-            cout << "Enter Salary: ";
-            cin >> e.Salary;
-            s.push(e);
-            break;
-        case 2:
-            cout << "Firing" << endl;
-            s.pop();
-            break;
-        case 3:
-            cout << "Showing" << endl;
-            s.print();
-            break;
-        default:
-            cout << "Invalid choice" << endl;
+        switch (choice) {
+            case 1:
+                cout << "Hiring" << endl;
+                cout << "Enter ID: ";
+                cin >> e.ID;
+                cout << "Enter Name: ";
+                cin >> e.Name;
+                cout << "Enter Salary: ";
+                cin >> e.Salary;
+                s.push(e);
+                break;
+            case 2:
+                cout << "Firing" << endl;
+                s.pop();
+                break;
+            case 3:
+                cout << "Showing" << endl;
+                s.print();
+                break;
+            case 0:
+                cout << "Exiting" << endl;
+                break;
+            default:
+                cout << "Invalid choice" << endl;
         }
     } while (choice != 0);
     cout << "Finished" << endl;
