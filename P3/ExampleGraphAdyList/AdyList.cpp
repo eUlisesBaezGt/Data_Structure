11a6a2b88d18f1d14e047bac95988a1704ab5873
kikinacademy <kikinacademy@gmail.com>
2022-11-10 11:20:58 -0600

    Ejemplo 3 grafos: MISSING COMPLETION

--- P3/ExampleGraphAdyList/AdyList.cpp
+++ P3/ExampleGraphAdyList/AdyList.cpp
@@ -7,6 +7,57 @@ ady_list::ady_list()
 	this->head_ = nullptr;
 }
 
+int ady_list::add_node(const char value)
+{
+	node* pos_node;
+
+	this->temp_ = new node; // Create a new node
+	this->temp_->nom = value;
+	this->temp_->next_node = nullptr;
+	this->temp_->next = nullptr;
+
+	if (!this->head_) // EMPTY GRAPH. NODE INSERTED
+	{
+		this->head_ = temp_;
+		return 0;
+	}
+
+	const int exists = search_node(value, pos_node);
+
+	if (exists == -1) // NODE DOESN'T EXISTS. INSERTED
+	{
+		pos_node->next_node = temp_;
+		return 0;
+	}
+
+	delete temp_; // NODE EXISTS. NOT INSERTED, MEMORY FREED
+	return -1;
+}
+
+int ady_list::add_edge(char org, char dest, int weight)
+{
+	int org_exists, dest_exists, ady_exists;
+	node *org_pos, *dest_pos;
+	edge* ady_pos;
+
+	if (!this->head_) // EMPTY GRAPH. EDGE NOT INSERTED
+		return -1;
+
+	org_exists = search_node(org, org_pos);
+	dest_exists = search_node(dest, dest_pos);
+
+	if (org_exists == -1 || dest_exists == -1) // NODES DOESN'T EXISTS. EDGE NOT INSERTED
+		return -1;
+
+	if(!org_pos->next) // EMPTY EDGES LIST
+	{
+		ady_pos = new edge;
+		this->ady_->dest = dest;
+		return 0;
+	}
+
+}
+
 void ady_list::show_full()
 {
 	if (!this->head_)
